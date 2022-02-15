271e96db7a40bf263c7215c859f20792ffb92168
xxjokerx <common.hori@protonmail.com>
2022-02-15 16:06:27 +0100

    First commit

---
+++ src/main/java/me/craicic/mvc/TrainingSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package me.craicic.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingSpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/craicic/mvc/TrainingSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.craicic.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
