9033893ab1ad442621581cba68ebfadb2e385749
xxjokerx <common.hori@protonmail.com>
2022-02-19 18:49:10 +0100

    Switch from no password encoding to a bcrypt encoding. Using Flyway to migrate our data.

---
+++ src/main/java/db/migration/V3__encode_password.java
@@ -0,0 +1,46 @@
+package db.migration;
+
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class V3__encode_password extends BaseJavaMigration {
+
+    private static final Logger logger = LoggerFactory.getLogger(V3__encode_password.class);
+
+    @Override
+    public void migrate(Context context) throws Exception {
+
+        Connection connection = context.getConnection();
+        Statement st = connection.createStatement();
+        ResultSet rs = st.executeQuery("SELECT id, password FROM app_user");
+        BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder();
+        final PreparedStatement psEncoded = connection.prepareStatement("UPDATE app_user SET password = ? WHERE id = ? ");
+
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String password = rs.getString("password");
+            try {
+                bcrypt.upgradeEncoding(password);
+                logger.debug("Password looks already encoded : " + password);
+            } catch (Exception e) {
+                logger.warn("Password was raw : " + password);
+                password = bcrypt.encode(password);
+                psEncoded.setString(1, password);
+                psEncoded.setInt(2,id);
+                psEncoded.execute();
+            }
+        }
+        rs.close();
+        psEncoded.close();
+        st.close();
+
+    }
+}
--- src/main/resources/db/migration/V2__fill_db.sql
+++ src/main/resources/db/migration/V2__fill_db.sql
@@ -1,5 +1,6 @@
 INSERT INTO app_user VALUES (1, 'Thorsten', 'notEncryptedPwd', date(timenow()));
 INSERT INTO app_user VALUES (2, 'Odin', 'stillNotEncryptedPwd', date(timenow()));
+INSERT INTO app_user VALUES (3, 'Loki', '$2y$10$JrOZDkeMN37.ePVmWgwyFeDNy2cbvOTfd06JFS473kfetmztYNf/.', date(timenow()));
 
 INSERT INTO entry VALUES (1, 1, 'thorben-janssen', date(timenow()), 'Thorsten59', 'azerty');
 INSERT INTO entry VALUES (2, 1, 'google', date(timenow()), 'ThorstenOnGoogle', 'pa$$w0rd');
